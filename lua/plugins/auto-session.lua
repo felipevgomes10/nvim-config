4c47ac66ea4381bda5a1c259717459770fb56797
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2025-01-03 18:42:39 -0300

    feat: add auto-session

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,25 @@
+return {
+  "rmagatti/auto-session",
+  lazy = false,
+  keys = {
+    { "<leader>wr", "<cmd>SessionSearch<CR>",         desc = "Session search" },
+    { "<leader>ws", "<cmd>SessionSave<CR>",           desc = "Save session" },
+    { "<leader>wa", "<cmd>SessionToggleAutoSave<CR>", desc = "Toggle autosave" },
+  },
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    session_lens = {
+      load_on_setup = true,
+      previewer = false,
+      mappings = {
+        delete_session = { "i", "<C-D>" },
+        alternate_session = { "n", "<C-S>" },
+        copy_session = { "i", "<C-Y>" },
+      },
+      theme_conf = {
+        border = true,
+      },
+    },
+  },
+}
