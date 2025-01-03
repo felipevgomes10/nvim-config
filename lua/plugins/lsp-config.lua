1a84c32de4c63fdbb5e8a30f8ea92e7a8a568790
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2025-01-03 18:45:15 -0300

    feat: add lsp and completions

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    config = function()
+      require("mason-lspconfig").setup({
+        auto_install = true,
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.bashls.setup({ capabilities = capabilities })
+      lspconfig.cssls.setup({ capabilities = capabilities })
+      lspconfig.tailwindcss.setup({ capabilities = capabilities })
+      lspconfig.html.setup({ capabilities = capabilities })
+      lspconfig.ts_ls.setup({ capabilities = capabilities })
+      lspconfig.jsonls.setup({ capabilities = capabilities })
+      lspconfig.remark_ls.setup({ capabilities = capabilities })
+      lspconfig.vacuum.setup({ capabilities = capabilities })
+      lspconfig.prismals.setup({ capabilities = capabilities })
+      lspconfig.sqlls.setup({ capabilities = capabilities })
+      lspconfig.ast_grep.setup({ capabilities = capabilities })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "I", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "C", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "R", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "T", vim.lsp.buf.type_definition, {})
+      vim.keymap.set("n", "D", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
