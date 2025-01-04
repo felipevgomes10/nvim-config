8ccf75347a76a585a524ce926b40f3524fd81e49
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2025-01-04 15:07:41 -0300

    feat: add vim.cmd configs

--- lua/nvim-config.lua
+++ lua/nvim-config.lua
@@ -2,6 +2,11 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set smartindent")
+vim.cmd("set smarttab")
+vim.cmd("set autoindent")
+vim.cmd("set number")
+vim.cmd("set nowrap")
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
