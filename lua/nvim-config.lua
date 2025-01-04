25dd3510de37677677b63d8cfec9f98e8a8fa164
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2025-01-04 15:25:28 -0300

    chore: format files

--- lua/nvim-config.lua
+++ lua/nvim-config.lua
@@ -12,51 +12,51 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Better window navigation
-vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Navigate windows left' })
-vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Navigate windows down' })
-vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Navigate windows up' })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Navigate windows right' })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Navigate windows left" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Navigate windows down" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Navigate windows up" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Navigate windows right" })
 
 -- Resize windows with arrows
-vim.keymap.set('n', 'H', ':vertical resize -2<CR>', { silent = true })
-vim.keymap.set('n', 'L', ':vertical resize +2<CR>', { silent = true })
+vim.keymap.set("n", "H", ":vertical resize -2<CR>", { silent = true })
+vim.keymap.set("n", "L", ":vertical resize +2<CR>", { silent = true })
 
 -- Better indenting
-vim.keymap.set('v', '<', '<gv', { desc = 'Indent left and reselect' })
-vim.keymap.set('v', '>', '>gv', { desc = 'Indent right and reselect' })
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
 
 -- Move text up and down
-vim.keymap.set('n', '<C-,>', ':m .+1<CR>==', { desc = 'Move line down' })
-vim.keymap.set('n', '<C-.>', ':m .-2<CR>==', { desc = 'Move line up' })
-vim.keymap.set('v', '<C-,>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
-vim.keymap.set('v', '<C-.>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+vim.keymap.set("n", "<C-,>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<C-.>", ":m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set("v", "<C-,>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "<C-.>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
 -- File operations
-vim.keymap.set('n', 'w', ':w<CR>', { desc = 'Save file' })
-vim.keymap.set('n', 'q', ':q<CR>', { desc = 'Quit' })
-vim.keymap.set('n', 'Q', ':q!<CR>', { desc = 'Quit without saving' })
-vim.keymap.set('n', 'wq', ':wq<CR>', { desc = 'Save and quit' })
-vim.keymap.set('n', 'wa', ':wa<CR>', { desc = 'Save all' })
-vim.keymap.set('n', 'qq', ':wqa<CR>', { desc = 'Save and quit all' })
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { desc = 'Clear highlights' })
+vim.keymap.set("n", "w", ":w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "q", ":q<CR>", { desc = "Quit" })
+vim.keymap.set("n", "Q", ":q!<CR>", { desc = "Quit without saving" })
+vim.keymap.set("n", "wq", ":wq<CR>", { desc = "Save and quit" })
+vim.keymap.set("n", "wa", ":wa<CR>", { desc = "Save all" })
+vim.keymap.set("n", "qq", ":wqa<CR>", { desc = "Save and quit all" })
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "Clear highlights" })
 
 -- Duplicate line
-vim.keymap.set('n', '<leader>d', ':t.<CR>', { desc = 'Duplicate line and paste below' })
-vim.keymap.set('n', '<leader>D', ':t-1<CR>', { desc = 'Duplicate line and paste above' })
-vim.keymap.set('v', '<leader>d', ':t+1<CR>gv=gv', { desc = 'Duplicate selection and paste below' })
-vim.keymap.set('v', '<leader>D', ':t-1<CR>gv=gv', { desc = 'Duplicate selection and paste above' })
+vim.keymap.set("n", "<leader>d", ":t.<CR>", { desc = "Duplicate line and paste below" })
+vim.keymap.set("n", "<leader>D", ":t-1<CR>", { desc = "Duplicate line and paste above" })
+vim.keymap.set("v", "<leader>d", ":t+1<CR>gv=gv", { desc = "Duplicate selection and paste below" })
+vim.keymap.set("v", "<leader>D", ":t-1<CR>gv=gv", { desc = "Duplicate selection and paste above" })
 
 -- Split window
-vim.keymap.set('n', '<leader>sv', ':vsplit<CR>', { desc = 'Split vertically' })
-vim.keymap.set('n', '<leader>sh', ':split<CR>', { desc = 'Split horizontally' })
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Split vertically" })
+vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Split horizontally" })
 
 -- Search improvements
-vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Next search result centered' })
-vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Previous search result centered' })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result centered" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result centered" })
 
 -- Quick pairs
-vim.keymap.set('i', '""', '""<left>', { desc = 'Add double quotes' })
-vim.keymap.set('i', "''", "''<left>", { desc = 'Add single quotes' })
-vim.keymap.set('i', '()', '()<left>', { desc = 'Add parentheses' })
-vim.keymap.set('i', '{}', '{}<left>', { desc = 'Add curly braces' })
-vim.keymap.set('i', '[]', '[]<left>', { desc = 'Add square brackets' })
+vim.keymap.set("i", '""', '""<left>', { desc = "Add double quotes" })
+vim.keymap.set("i", "''", "''<left>", { desc = "Add single quotes" })
+vim.keymap.set("i", "()", "()<left>", { desc = "Add parentheses" })
+vim.keymap.set("i", "{}", "{}<left>", { desc = "Add curly braces" })
+vim.keymap.set("i", "[]", "[]<left>", { desc = "Add square brackets" })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,41 +1,41 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('telescope').setup({
-        defaults = {
-          layout_strategy='vertical',
-          layout_config = {
-            vertical = {
-              width = 0.9
-            }
-          }
-        }
-      })
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					layout_strategy = "vertical",
+					layout_config = {
+						vertical = {
+							width = 0.9,
+						},
+					},
+				},
+			})
 
-      local builtin = require('telescope.builtin')
+			local builtin = require("telescope.builtin")
 
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-      vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope find files in git' })
-    end
-  },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({})
-          }
-        }
-      })
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+			vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Telescope find files in git" })
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
 
-      require("telescope").load_extension("ui-select")
-    end
-  }
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
